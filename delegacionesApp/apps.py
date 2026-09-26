8d1ddbac3e1db5c336828c12fb686dc4b7fd047d
KaythFK <kathalyna.higuera@inacapmail.cl>
2026-09-26 00:01:25 -0300

    eliminacion de agendaApp

--- delegacionesApp/apps.py
+++ delegacionesApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AgendaappConfig(AppConfig):
+class DelegacionesappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agendaApp'
+    name = 'delegacionesApp'
